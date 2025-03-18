2276410c84e25069db4f2f13050a26a57eaba0d8
Jos <jpkotze148@gmail.com>
2025-03-18 21:11:04 +0200

    allowAnonymous on Inedx action, but auth on base controller

--- Strava2ExcelWepApiBackend/Controllers/BaseApiController.cs
+++ Strava2ExcelWepApiBackend/Controllers/BaseApiController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Strava2ExcelWebApiBackend.Controllers
 {
-    
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
--- Strava2ExcelWepApiBackend/Controllers/FallbackController.cs
+++ Strava2ExcelWepApiBackend/Controllers/FallbackController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Strava2ExcelWebApiBackend.Controllers
 {
     public class FallbackController : BaseApiController
     {
         [HttpGet]
+        [AllowAnonymous]
         public ActionResult Index()
         {
             return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html"), "text/HTML");
