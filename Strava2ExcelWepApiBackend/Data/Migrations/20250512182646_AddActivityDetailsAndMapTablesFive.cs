158b028a41291d2879aad6c345b333c47fa7b1ac
Jos <jpkotze148@gmail.com>
2025-05-12 22:24:40 +0200

    Modified Id for activity model to be long instead of int

---
+++ Strava2ExcelWepApiBackend/Data/Migrations/20250512182646_AddActivityDetailsAndMapTablesFive.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Strava2ExcelWebApiBackend.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddActivityDetailsAndMapTablesFive : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Activities",
+                table: "Activities");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "Activities",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Activities",
+                table: "Activities",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Activities",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+    }
+}
--- Strava2ExcelWepApiBackend/Models/Activity.cs
+++ Strava2ExcelWepApiBackend/Models/Activity.cs
@@ -2,7 +2,7 @@
 {
     public class Activity
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public long ActivityId { get; set; }
         public int UserId { get; set; }
         public string? Pace { get; set; }
